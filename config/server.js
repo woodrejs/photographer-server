2c0d660c715f5f8cc90e9d619b2fbecd46a95d40
woodrejs <woodrejs@gmail.com>
2021-11-11 15:06:56 +0100

    solve auth bug

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
+  url: env("URL"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '62663c7bea1fa44cdd8b5fb9ba89c68b'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
